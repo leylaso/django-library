6c329c130319ae45dfa161ed5b472dc9f0d81489
Sofian Benaissa <sofian@koumbit.org>
2011-09-19 16:04:21 -0400

    Rework filtered books view substantually

--- library/views.py
+++ library/views.py
@@ -40,7 +40,7 @@ def booksBy(request, args=''):
     bookQuery = 'Book.objects.filter(' + filters + ')'
   else: 
     bookQuery = 'Book.objects.all()'
-  exec "books = " + bookQuery
+  exec "books = " + bookQuery + '.order_by("id").reverse()'
 
   pagedBook = Paginator(books, 50)
   curPage = request.GET.get('page')
@@ -55,39 +55,44 @@ def booksBy(request, args=''):
   else:
     bookPage = pagedBook.page(1)
 
+  actives = {}
+
   language = {}
-  for book in books:
-    if language.has_key(book.language):
-      language[book.language]['count'] += 1
-    else:
-      language[book.language]= {'count': 1, 'text': book.language, 'link': '/bk' + args + '/language:' + book.language}
+  if active.has_key('language'):
+    language = None
+    actives['language'] = {'text': active['language']}
+  else:
+    exec "langs = " + bookQuery + ".values('language').order_by('language').annotate(Count('title'))"
+    for lang in langs:
+      language[lang['language']] = {'count': lang['title__count'], 'text': lang['language'], 'link': '/bk' + args + '/language:' + lang['language']}
 
   category = {}
-  for book in books:
-    if category.has_key(str(book.category.id)):
-      category[str(book.category.id)]['count'] += 1
-    else:
-      category[str(book.category.id)] = {'count': 1, 'text': book.category.title, 'link': '/bk' + args + '/category:' + str(book.category.id)}
+  if active.has_key('category'):
+    category = None
+    cat = Category.objects.get(id=active['category'])
+    actives['category'] = {'text': cat.title, 'link': '/bk' + args + '/category:' + str(cat.id)}
+  else:
+    exec "cats = " + bookQuery + ".values('category__id', 'category__title').order_by('category__title').annotate(Count('title'))"
+    for cat in cats:
+      category[cat['category__title']] = {'count': cat['title__count'], 'text': cat['category__title'], 'link': '/bk' + args + '/category:' + str(cat['category__id'])}    
 
   author = {}
-  for book in books:
-    if author.has_key(str(book.author.id)):
-      author[str(book.author.id)]['count'] += 1
-    else:
-      author[str(book.author.id)] = {'count': 1, 'text': book.author.__unicode__, 'link': '/bk' + args + '/author:' + str(book.author.id)}
+  if active.has_key('author'):
+    category = None
+    auth = Author.objects.get(id=active['author'])
+    actives['author'] = {'text': auth.__unicode__, 'link': '/bk' + args + '/category:' + str(auth.id)}
+  else:
+    exec "auths = " + bookQuery + ".values('author__id', 'author__surname', 'author__givenames').order_by('author__givenames').annotate(Count('title'))"
+    for auth in auths:
+      authName = auth['author__givenames'] + ' ' + auth['author__surname']
+      author[authName] = {'count': auth['title__count'], 'text': authName, 'link': '/bk' + args + '/author:' + str(auth['author__id'])}
   
-  for act, val in active.items():
-    exec 'active[act] = ' + act + '["' + active[act] + '"]'
-    active[act]['cond'] = val
-    exec act + ' = None'
   for act in active:
     link = '/bk'
-    for a in active:
+    for a, val in active.items():
       if a != act:
-        link += '/' + a + ':' + active[a]['cond']
-    active[act]['link'] = link
-
-  
+        link += '/' + a + ':' + val
+    actives[act]['link'] = link
 
   return render_to_response('library/bookSearch.html',
-    {'books': bookPage, 'langs': language, 'cats': category, 'auths': author, 'active': active})
+    {'books': bookPage, 'langs': language, 'cats': category, 'auths': author, 'active': actives})
