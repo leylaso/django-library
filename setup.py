7faf2fa2956940cbc0d750b743351c71e7d721a1
Guillaume! <assoupis@host248.office.koumbit.net>
2013-12-28 21:28:40 -0500

    Actually really included the static files for the django package.

--- setup.py
+++ setup.py
@@ -2,26 +2,15 @@
 
 # We're using the examples from http://docs.python.org/2/distutils/setupscript.html
 
+import os
 from distutils.core import setup
-
-def add_folder(source):
-    s = set()
-    for path in os.listdir(source):
-        tpath = os.path.join(source,path)
-        if os.path.isfile(tpath):
-            s.add(tpath)
-        elif os.path.isdir(tpath):
-            s = s.union(add_folder(tpath))
-    return list(s)
 
 setup(name='django-library',
       version='1.0.1', 					# errr http://semver.org/
       description='a django library management system',
       author='Guillaume Le Punk',
       author_email='gbeaulieu@koumbit.org',
-      data_files = [
-        ('/usr/share/django-library/library/static', add_folder(os.path.abspath('django-library/library/static'))),
-        ],
-      url='https://github.com/GuillaumeFromage/django-library',
       packages=['django-library', 'django-library.library'],
+      url='https://github.com/GuillaumeFromage/django-library',
+      package_data = { 'django-library.library': ['static/imgs/*', 'static/css/*'] }
      )
