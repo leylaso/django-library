c2c745ef6505728e41bd7a63d34707f6fde79146
Guillaume! <gbeaulieu@koumbit.org>
2013-12-31 14:55:18 -0500

    If we use the install method described in https://github.com/felipeborges/python-openlibrary we just don't need to copy the file just to import openlibrary
    It sets all the values for the fields coming from openlibrary to undefined at the moment, so wait further commits before applying this patch

--- django-library/library/views.py
+++ django-library/library/views.py
@@ -8,7 +8,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.template import RequestContext
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.conf import settings
-from library.openlibrary import openlibrary
+import openlibrary
 import json
 import datetime
 import string
